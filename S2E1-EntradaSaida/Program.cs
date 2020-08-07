8eedb6315479dae2a332ebc371b3ec15b41f60ce
Thiago Nascimento <t.nascimento.work@gmail.com>
2020-08-07 20:28:49 -0300

    Add project files.

---
+++ S2E1-EntradaSaida/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace S2E1_EntradaSaida
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string EntradaNome, EntradaIdade;
+
+            Console.Write("Digite o seu nome: ");
+            EntradaNome = Console.ReadLine();
+            Console.Write("Digite a sua idade: ");
+            EntradaIdade = Console.ReadLine();
+
+            int IdadeNumero = int.Parse(EntradaIdade);
+            int AnoNascimento =  DateTime.Now.Year - IdadeNumero;
+
+            Console.WriteLine("--------------------------------------------");
+
+            Console.WriteLine("Seu nome é: " + EntradaNome);
+            Console.WriteLine("O Ano aproximado de seu nascimento é: " + AnoNascimento + ", e sua idade é de: " + EntradaIdade);
+
+            Console.ReadKey();
+        }
+    }
+}
